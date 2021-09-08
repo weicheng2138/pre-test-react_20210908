ba89cad90b046d44379d8c98da43cc1206b431f1
weicheng <weicheng2138@gmail.com>
2021-09-08 12:26:00 +0800

    add try and catch for api from server in down

--- src/hooks/useBackendApi.js
+++ src/hooks/useBackendApi.js
@@ -47,49 +47,54 @@ const useBackendApi = () => {
 
     const fetchData = useCallback((payload) => {
         const fetchingData = async () => {
-            const rawResponse = await fetch("http://localhost:4000/post", {
-                method: "POST",
-                headers: {
-                    // Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(newQueryItem(payload)),
-            });
-            // console.log(await rawResponse.text());
-            const tempContent = await rawResponse.text();
-            const sanitized = "[" + tempContent.replace(/}{/g, "},{") + "]";
-            const content = JSON.parse(sanitized);
-            // console.log(content);
-            // console.log(payload);
-            content.sort((a, b) => {
-                return a.id - b.id;
-            });
+            try {
+                const rawResponse = await fetch("http://localhost:4000/post", {
+                    method: "POST",
+                    headers: {
+                        // Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(newQueryItem(payload)),
+                });
+                // console.log(await rawResponse.text());
+                const tempContent = await rawResponse.text();
+                const sanitized = "[" + tempContent.replace(/}{/g, "},{") + "]";
+                const content = JSON.parse(sanitized);
+                // console.log(content);
+                // console.log(payload);
+                content.sort((a, b) => {
+                    return a.id - b.id;
+                });
 
-            const filteredContent = content.filter((el, index, array) => {
-                if (index % 2 === 0) {
-                    return el.price < array[index + 1].price;
-                } else {
-                    return el.price < array[index - 1].price;
-                }
-            });
-            // console.log(filteredContent);
-            // console.log(payload);
-            setFetchedData(
-                filteredContent.map((el, index) => {
-                    return {
-                        ticketType: payload[index].ticketType,
-                        startStaName: el.startStaCode,
-                        endStaName: el.endStaCode,
-                        trnclassCode: payload[index].trnclassCode,
-                        mileage: el.mileage,
-                        ticketPriceType: payload[index].ticketPriceType,
-                        price: el.price,
-                        count: el.count,
-                        subtotal: el.subtotal,
-                    };
-                })
-            );
-            setIsLoading(false);
+                const filteredContent = content.filter((el, index, array) => {
+                    if (index % 2 === 0) {
+                        return el.price < array[index + 1].price;
+                    } else {
+                        return el.price < array[index - 1].price;
+                    }
+                });
+                // console.log(filteredContent);
+                // console.log(payload);
+                setFetchedData(
+                    filteredContent.map((el, index) => {
+                        return {
+                            ticketType: payload[index].ticketType,
+                            startStaName: el.startStaCode,
+                            endStaName: el.endStaCode,
+                            trnclassCode: payload[index].trnclassCode,
+                            mileage: el.mileage,
+                            ticketPriceType: payload[index].ticketPriceType,
+                            price: el.price,
+                            count: el.count,
+                            subtotal: el.subtotal,
+                        };
+                    })
+                );
+                setIsLoading(false);
+            } catch (err) {
+                alert(err);
+                setIsLoading(false);
+            }
         };
 
         setIsLoading(true);
